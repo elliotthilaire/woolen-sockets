e03dba44722923558ec8926f9e9f2182bc4bdcbf
Elliott Hilaire <elliott.hilaire@gmail.com>
2017-03-17 16:57:11 +0000

    Set up new_player channel

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -15,4 +15,14 @@ defmodule PhoenixPhaser.RoomChannel do
     broadcast_from socket, "update_world", %{ x: x, y: y}
     {:noreply, socket}
   end
+
+  def handle_in("new_player", %{"player" => player, "position" => %{"x" => x, "y" => y}}, socket) do
+    PhoenixPhaser.GameState.put(player, %{x: x, y: y})
+    new_player = %{player_id: player, position: %{x: x, y: y}}
+    IO.inspect(new_player)
+
+    broadcast_from socket, "new_player", new_player
+    {:noreply, socket}
+  end
+
 end
