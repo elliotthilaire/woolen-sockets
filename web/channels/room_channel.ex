0e9890bf9b3a6fb03e25d1329620eab9bada6736
Elliott Hilaire <elliott.hilaire@gmail.com>
2017-03-17 21:09:50 +0000

    Make multiplayer work

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -13,16 +13,18 @@ defmodule PhoenixPhaser.RoomChannel do
     PhoenixPhaser.GameState.put(player, %{x: x, y: y})
     world = PhoenixPhaser.GameState.get
 
-    broadcast_from socket, "update_world", world
+    broadcast! socket, "update_world", world
     {:noreply, socket}
   end
 
-  def handle_in("new_player", %{"player" => player, "position" => %{"x" => x, "y" => y}}, socket) do
+  def handle_in("im_new_here", %{"player" => player, "position" => %{"x" => x, "y" => y}}, socket) do
     PhoenixPhaser.GameState.put(player, %{x: x, y: y})
     new_player = %{player_id: player, position: %{x: x, y: y}}
+    world = PhoenixPhaser.GameState.get
     IO.inspect(new_player)
 
-    broadcast_from socket, "new_player", new_player
+    broadcast! socket, "new_player_joined", new_player
+    broadcast! socket, "hello_world", world
     {:noreply, socket}
   end
 
