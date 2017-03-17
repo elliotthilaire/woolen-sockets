8e247079573c451afe19f5e680aa344909070f7f
Elliott Hilaire <elliott.hilaire@gmail.com>
2017-03-17 09:02:18 +0000

    Modify config/dev.exs

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,6 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :phoenix_phaser, PhoenixPhaser.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "phoenix_phaser_dev",
   hostname: "localhost",
   pool_size: 10
