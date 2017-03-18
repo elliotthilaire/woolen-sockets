b4b44011658f0eb8a05e710ddd032ebe3485dd2b
Elliott Hilaire <elliott.hilaire@gmail.com>
2017-03-18 10:57:15 +0000

    Use GenServer for WorldBroadcaster
    Before it was blocking commands in iex. Now it does not.

--- lib/phoenix_phaser/world_broadcaster.ex
+++ lib/phoenix_phaser/world_broadcaster.ex
@@ -1,20 +1,31 @@
 defmodule PhoenixPhaser.WorldBroadcaster do
+  use GenServer
+
+  # 16 milliseconds is (60fps)
+  @delay_time 16
 
   def start_link do
-    world = PhoenixPhaser.GameState.get
-    broadcast_world(world)
+    GenServer.start_link(__MODULE__, %{})
   end
 
-  def broadcast_world(world) do
+  def init(_state) do
+    world = PhoenixPhaser.GameState.get
+
+    Process.send_after(self(), :work, @delay_time)
+    {:ok, world}
+  end
+
+  def handle_info(:work, existing_world) do
     new_world = PhoenixPhaser.GameState.get
 
-    unless Map.equal?(world, new_world) do
+    unless Map.equal?(existing_world, new_world) do
       IO.inspect(new_world)
       PhoenixPhaser.Endpoint.broadcast!("room:lobby", "update_world", new_world)
     end
 
-    Process.sleep(16)
-    broadcast_world(new_world)
-  end
+    # Start the timer again
+    Process.send_after(self(), :work, @delay_time)
 
+    {:noreply, new_world}
+  end
 end
